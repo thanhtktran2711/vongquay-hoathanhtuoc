10d4201fc5d584428d4283eacf6ba9a833634c16
thanhtktran2711 <thanh.tk.tran2711@gmail.com>
2026-07-16 14:41:04 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+    apiUrl: "https://script.google.com/macros/s/AKfycbw3bB15IaYeH7h6WI4yfQgQ0eAO6AuHz0-ukYH8Jt8XEquW9qCdnmMWgjCcN7F-Z4BU/exec",
+
+    prizes: [
+        "Giảm 20.000đ",
+        "Voucher 5%",
+        "Voucher 10%",
+        "Giảm 100.000đ",
+        "Freeship",
+        "1 SP Hoa/Lá",
+        "Free SP thấp nhất",
+        "Chúc may mắn"
+    ]
+};
